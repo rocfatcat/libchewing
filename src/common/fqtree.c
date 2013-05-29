eb06ed3cb82ec814ad4840a6cbf56dd1466521b0
ChangZhuo Chen <czchen@gmail.com>
2013-05-30 00:08:07 +0800

    Update fqtree.c

--- src/common/fqtree.c
+++ src/common/fqtree.c
@@ -8,4 +8,30 @@
  * of this file.
  */
 
+#include <assert.h>
+#include <stdio.h>
+
 #include "fqtree.h"
+#include "chewing-private.h"
+
+int serialize_fqforest(struct FQForest *forest, const char *filename)
+{
+	FILE *fd;
+	uint32_t current;
+	int i;
+
+	assert(forest);
+
+	fd = fopen(filename, "w");
+	if (!fd) {
+		return -1;
+	}
+
+	fseek(fd, MAX_FQTREE_PHRASE_LEN * sizeof(uint32_t), SEEK_SET);
+	for (i = 0; i < MAX_FQTREE_PHRASE_LEN; ++i) {
+		current = ftell(fd);
+		pwrite(fd, &current, sizeof(current), i * sizeof(uint32_t));
+	}
+
+	fclose(fd);
+}
