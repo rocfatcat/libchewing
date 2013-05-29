688d4927622606b86bfc0ee59a636b4a0e98d1b7
ChangZhuo Chen <czchen@gmail.com>
2013-05-29 23:12:05 +0800

    Add fqtree.[ch]

---
+++ include/internal/fqtree.h
@@ -0,0 +1,45 @@
+/**
+ * fqtree.h
+ *
+ * Copyright (c) 2013
+ *	libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+
+#ifdef HAVE_CONFIG_H
+#  include <config.h>
+#endif
+
+#ifdef HAVE_INTTYPES_H
+#  include <inttypes.h>
+#elif defined HAVE_STDINT_H
+#  include <stdint.h>
+#endif
+
+#include "chewing-private.h"
+
+#define FQTREE_PHRASE_LEN (6)
+
+struct FQTreeNode {
+	enum { INTERNAL_NODE, LEAF_NODE } type;
+	union {
+		struct {
+			uint32_t children[FQTREE_PHRASE_LEN * ZUIN_SIZE + 1];
+		} internal;
+		struct {
+			uint32_t phrase_index[FQTREE_PHRASE_LEN * ZUIN_SIZE + 1];
+		} leaf;
+	};
+};
+
+struct FQTree {
+	uint32_t root;
+	uint32_t depth;
+	uint32_t key_phrase_index[];
+};
+
+struct FQForest {
+	struct FQTree *fqtree[FQTREE_PHRASE_LEN];
+};
---
+++ src/common/fqtree.c
@@ -0,0 +1,11 @@
+/**
+ * fqtree.c
+ *
+ * Copyright (c) 2013
+ *	libchewing Core Team. See ChangeLog for details.
+ *
+ * See the file "COPYING" for information on usage and redistribution
+ * of this file.
+ */
+
+#include "fqtree.h"
